3afbaa0f6d829563c06da2e6ca89611551541b64
Lewvy <aditya29december@gmail.com>
2024-08-10 00:39:45 +0530

    lua/plugins/fugitive.lua

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -1,48 +1,52 @@
 return {
 	{
-		'neovim/nvim-lspconfig',
+		"neovim/nvim-lspconfig",
 		dependencies = {
-			'williamboman/mason.nvim',
-			'williamboman/mason-lspconfig.nvim',
-			'WhoIsSethDaniel/mason-tool-installer.nvim',
-			{ 'j-hui/fidget.nvim', opts = {} },
-			{ 'folke/neodev.nvim', opts = {} },
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			{ "j-hui/fidget.nvim", opts = {} },
+			{ "folke/neodev.nvim", opts = {} },
 		},
 		config = function()
 			-- Safe loading of modules
-			local lspconfig = require('lspconfig')
-			local mason = require('mason')
-			local mason_lspconfig = require('mason-lspconfig')
-			local mason_tool_installer = require('mason-tool-installer')
-			local cmp_nvim_lsp = require('cmp_nvim_lsp')
+			local lspconfig = require("lspconfig")
+			local mason = require("mason")
+			local mason_lspconfig = require("mason-lspconfig")
+			local mason_tool_installer = require("mason-tool-installer")
+			local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 			-- Autocommand setup
-			vim.api.nvim_create_autocmd('LspAttach', {
-				group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 				callback = function(event)
 					local map = function(keys, func, desc)
-						vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 					end
 
-					map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-					map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-					map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-					map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-					map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-					map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-					map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-					map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-					map('K', vim.lsp.buf.hover, 'Hover Documentation')
-					map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+					map(
+						"<leader>ws",
+						require("telescope.builtin").lsp_dynamic_workspace_symbols,
+						"[W]orkspace [S]ymbols"
+					)
+					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+					map("K", vim.lsp.buf.hover, "Hover Documentation")
+					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
 					if client and client.server_capabilities.documentHighlightProvider then
-						vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 							buffer = event.buf,
 							callback = vim.lsp.buf.document_highlight,
 						})
 
-						vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
 							buffer = event.buf,
 							callback = vim.lsp.buf.clear_references,
 						})
@@ -52,13 +56,13 @@ return {
 
 			-- LSP capabilities and server setup
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend('force', capabilities, cmp_nvim_lsp.default_capabilities())
+			capabilities = vim.tbl_deep_extend("force", capabilities, cmp_nvim_lsp.default_capabilities())
 			local servers = {
 				lua_ls = {
 					settings = {
 						Lua = {
 							completion = {
-								callSnippet = 'Replace',
+								callSnippet = "Replace",
 							},
 						},
 					},
@@ -70,33 +74,33 @@ return {
 
 			-- Ensure servers and formatters are installed
 			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, { 'stylua' })
-			mason_tool_installer.setup { ensure_installed = ensure_installed }
+			vim.list_extend(ensure_installed, { "stylua" })
+			mason_tool_installer.setup({ ensure_installed = ensure_installed })
 
 			-- Mason LSPconfig setup
-			mason_lspconfig.setup {
+			mason_lspconfig.setup({
 				handlers = {
 					function(server_name)
 						local server = servers[server_name] or {}
-						server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
 						lspconfig[server_name].setup(server)
 					end,
 				},
-			}
+			})
 		end,
 	},
 
 	{
-		'stevearc/conform.nvim',
+		"stevearc/conform.nvim",
 		lazy = false,
 		keys = {
 			{
-				'<leader>f',
+				"<leader>f",
 				function()
-					require('conform').format { async = true, lsp_fallback = true }
+					require("conform").format({ async = true, lsp_fallback = true })
 				end,
-				mode = '',
-				desc = '[F]ormat buffer',
+				mode = "",
+				desc = "[F]ormat buffer",
 			},
 		},
 		opts = {
@@ -109,7 +113,9 @@ return {
 				}
 			end,
 			formatters_by_ft = {
-				lua = { 'stylua' },
+				lua = { "stylua" },
+				c = { "clang-format" },
+				go = { "gofumpt" },
 			},
 		},
 	},
